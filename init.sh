d566800818937066081bdd81c35346f866fa1928
tong92 <tong92power@gmail.com>
2023-07-29 00:57:14 +0900

    Update initial script
    Add nvim connect to vimrc script

--- init.sh
+++ init.sh
@@ -5,3 +5,9 @@
 
 ln -sf ${PWD}/.vimrc ~/.vimrc 
 
+mkdir ~/.config
+mkdir ~/.config/nvim
+touch ~/.config/nvim/init.vim
+echo "set runtimepath+=~/.vim,~/.vim/after" >> ~/.config/nvim/init.vim
+echo "set packpath+=~/.vim" >> ~/.config/nvim/init.vim
+echo sou"rce ~/.vimrc" >> ~/.config/nvim/init.vim
